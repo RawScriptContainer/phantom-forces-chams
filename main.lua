509c942602ec883082d0df2235aeb4f3bba8be1d
wally <38594850+wally-rblx@users.noreply.github.com>
2021-01-18 19:34:12 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-if (not (syn and syn.protect_gui)) or (not get_hidden_gui) then
+if (not (syn and syn.protect_gui)) and (not get_hidden_gui) then
     return
 end
 
 local maid = loadstring(game:HttpGet('https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Maid.lua'))()
 local signal = loadstring(game:HttpGet('https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Signal.lua'))()
-local material = loadstring(game:HttpGet('https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua'))()
+local library = loadstring(game:HttpGet('https://pastebin.com/raw/edJT9EGX'))()
 
 local replication; do
     for _, tbl in next, getgc(true) do
@@ -69,13 +69,20 @@ local characterList = getupvalue(replication.getplayerhit, 1)
 local characterAdded = signal.new()
 local transparencyChanged = signal.new();
 local chamStateChanged = signal.new();
+local teamStateChanged = signal.new();
 
-shared.transparency = 0;
-shared.chams = false;
 
 characterAdded:Connect(function(player, character) 
     local maid = playerMaids[player]
     local chams = {}
+
+    local team = player.Team
+    local isSameTeam = (client.Team == team);
+
+    local isVisible = library.flags.chams
+    if (not library.flags.showTeam) and isSameTeam then
+        isVisible = false;
+    end
 
     for _, part in next, character:GetChildren() do
         if part:IsA('BasePart') and part.Transparency ~= 1 then
@@ -86,8 +93,8 @@ characterAdded:Connect(function(player, character)
                 AlwaysOnTop = true,
                 Color3 = player.TeamColor.Color;
                 Size = (part.Size + Vector3.new(0.5, 0.5, 0.5)),
-                Transparency = (shared.transparency or 0),
-                Visible = (shared.chams or false),
+                Transparency = (library.flags.chamsTransparency or 0),
+                Visible = isVisible,
 
                 ZIndex = 10;
                 Parent = folder,
@@ -102,6 +109,22 @@ characterAdded:Connect(function(player, character)
         end
     end))
 
+    maid:GiveTask(teamStateChanged:Connect(function(state) 
+        -- recalculating for if localplayer changes team :D
+        local team = player.Team
+        local isSameTeam = (client.Team == team);
+
+        local isVisible = library.flags.chams
+        if (not library.flags.showTeam) and isSameTeam then
+            isVisible = false;
+        end
+
+        for _, part in next, chams do
+            part.Visible = isVisible
+            part.Color3 = player.TeamColor.Color;
+        end
+    end))
+
     maid:GiveTask(transparencyChanged:Connect(function(new)
         for _, part in next, chams do
             part.Transparency = new;
@@ -113,6 +136,10 @@ characterAdded:Connect(function(player, character)
             part:Destroy()
         end
     end)
+
+    maid:GiveTask(player:GetPropertyChangedSignal('Team'):connect(function()
+        teamStateChanged:Fire(library.flags.showTeams)
+    end))
 
     maid:GiveTask(character.AncestryChanged:connect(function(_, new)
         if new == nil then
@@ -178,53 +205,42 @@ for _, team in next, workspace.Players:GetChildren() do
     end
 end
 
-local ui = material.Load({
-    Title = 'Phantom Forces - Chams',
-    Style = 2,
-    SizeY = 150,
-    Theme = 'Dark',
-})
+-- forces chams to refresh if client changes teams (e.g. in a vip server)
+client:GetPropertyChangedSignal('Team'):connect(function()
+    teamStateChanged:Fire(library.flags.showTeams)
+end)
 
-local tab = ui.New({
-    Title = 'Main',
-})
+local window = library:CreateWindow('Phantom Forces');
 
-tab.Toggle({
-    Text = 'Chams',
-    Callback = function(state)
-        shared.chams = state;
+window:AddToggle({
+    text = 'Chams', 
+    flag = 'chams', 
+    callback = function(state)
         chamStateChanged:Fire(state)
+    end
+})
+
+window:AddToggle({
+    text = 'Show Teammates',
+    flag = 'showTeam',
+    callback = function(state)
+        teamStateChanged:Fire(state)
     end,
 })
 
-tab.Slider({
-    Text = 'Transparency',
-    Min = 0,
-    Max = 10,
-    Def = 0,
-    Callback = function(new)
-        shared.transparency = new/10
-        transparencyChanged:Fire(shared.transparency)
-    end,
+window:AddSlider({
+    text = 'Transparency',
+    flag = 'chamsTransparency',
+    min = 0,
+    max = 1,
+    float = 0.01,
+    callback = function(value)
+        transparencyChanged:Fire(value)
+    end
 })
 
-tab.Button({
-    Text = 'Credits',
-    Menu = {
-        Scripting = function()
-            ui.Banner({
-                Text = 'BigTimbob @ v3rmillion.net',
-            })
-        end,
-        Libraries = function()
-            ui.Banner({
-                Text = 'Quenty - Nevermore Engine (Signal & Maids)',
-            })
-        end,
-        Interface = function()
-            ui.Banner({
-                Text = 'aKinlei - Material Lua'
-            })
-        end,
-    }
-})
+window:AddLabel({text = 'Credits'})
+window:AddLabel({text = 'Scripting: wally'})
+window:AddLabel({text = 'Interface - Jan'})
+window:AddLabel({text = 'Libraries - Quenty'})
+library:Init()
